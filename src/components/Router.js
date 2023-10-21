7a96eb3213e675e55e2798b29b63457741567b51
steve040315 <steve040315@gmail.com>
2023-10-21 11:40:37 +0900

    ...

---
+++ src/components/Router.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "../routes/Auth"
+import Home from "../routes/Home"
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={isLoggedIn ? <Home /> : <Auth />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
